fdef65cf7d6e4a1675ce7fae74a14b5306505925
aseof54 <ivan.cuh2019@gmail.com>
2026-05-21 20:46:26 +0300

    color mutation addad

--- src/components/GridCell.jsx
+++ src/components/GridCell.jsx
@@ -1,11 +1,11 @@
 import FlowerTile from "../assets/flowertile.svg?react";
 import Empty from '../assets/empty.png';
 
-const GridCell = ({ state, onClick }) => {
+const GridCell = ({ state, onClick, tileColor }) => {
   return (
     <button className='gridcell' onClick={onClick}>
       {state ? (
-        <FlowerTile />
+        <FlowerTile fill={tileColor} stroke={tileColor}/>
       ) : (
         <img src={Empty} alt="Empty tile" />
       )}
