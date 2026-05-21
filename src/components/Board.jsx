dda16ec67a2e22145ed6b011855b76956a29ff7e
aseof54 <ivan.cuh2019@gmail.com>
2026-05-21 20:03:01 +0300

    flower grow

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,19 +1,75 @@
+import React, { useState, useEffect } from 'react'
 import GridCell from './GridCell'
 
 const Board = () => {
-    let board = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => true))
+  const [board, setBoard] = useState(() => {
+    return Array.from({ length: 10 }, (_, rowIndex) =>
+      Array.from({ length: 10 }, (_, cellIndex) => {
+        return rowIndex === 5 && cellIndex === 5 ? true : false
+      })
+    )
+  })
 
-    // board = board.map()
-    console.log(board)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBoard((currentBoard) => {
+        const nextBoard = currentBoard.map((row) => [...row])
+        let hasChanges = false
+
+        for (let r = 0; r < 10; r++) {
+          for (let c = 0; c < 10; c++) {
+            if (currentBoard[r][c] === true) {
+              for (let dr = -1; dr <= 1; dr++) {
+                for (let dc = -1; dc <= 1; dc++) {
+                  if (dr === 0 && dc === 0) continue
+
+                  const neighborRow = r + dr
+                  const neighborCell = c + dc
+
+                  if (
+                    neighborRow >= 0 && neighborRow < 10 &&
+                    neighborCell >= 0 && neighborCell < 10
+                  ) {
+                    if (currentBoard[neighborRow][neighborCell] === false) {
+                      nextBoard[neighborRow][neighborCell] = true
+                      hasChanges = true
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        return hasChanges ? nextBoard : currentBoard
+      })
+    }, 2500)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const handleCellClick = (clickedRow, clickedCell) => {
+    setBoard((currentBoard) => {
+      const nextBoard = currentBoard.map((row) => [...row]);
+      nextBoard[clickedRow][clickedCell] = !nextBoard[clickedRow][clickedCell];
+      return nextBoard;
+    });
+  };
+
   return (
     <div className="board">
-        {board.map((row)=>{
-            return row.map((cell)=>{
-                return <GridCell state={cell}></GridCell>
-            })
-        })}
+      {board.map((row, rowIndex) => {
+        return row.map((cell, cellIndex) => {
+          return (
+            <GridCell 
+              key={`${rowIndex}-${cellIndex}`} 
+              state={cell}
+              onClick={() => handleCellClick(rowIndex, cellIndex)} 
+            />
+          )
+        })
+      })}
     </div>
-    )
+  )
 }
 
 export default Board
--- src/components/GridCell.jsx
+++ src/components/GridCell.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
 import FlowerTile from "../assets/flowertile.svg?react";
-import Empty from '../assets/empty.png'
-const GridCell = ({state = true}) => {
-    const [flower, isflower] = useState(state);
-    const handleClick = (event) => {
-        event.preventDefault();
-        isflower((prevflower) => !prevflower);
-    };
-  return (
-        <button className='gridcell' onClick={handleClick}>
-            {/* <img src={flower ? FlowerTile : Empty} alt="" /> */}
-            {<FlowerTile></FlowerTile>}
-        </button>
-  )
-}
+import Empty from '../assets/empty.png';
 
-export default GridCell
+const GridCell = ({ state, onClick }) => {
+  return (
+    <button className='gridcell' onClick={onClick}>
+      {state ? (
+        <FlowerTile />
+      ) : (
+        <img src={Empty} alt="Empty tile" />
+      )}
+    </button>
+  );
+};
+
+export default GridCell;
